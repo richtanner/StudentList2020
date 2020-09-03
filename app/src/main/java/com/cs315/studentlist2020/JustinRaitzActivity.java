8ffb22c7037d6d807cd2093e32ca920b4149f9de
Justin Raitz <jmr18c@acu.edu>
2020-09-03 12:48:11 -0500

    Added ideas to the array and created a ListView for them

--- app/src/main/java/com/cs315/studentlist2020/JustinRaitzActivity.java
+++ app/src/main/java/com/cs315/studentlist2020/JustinRaitzActivity.java
@@ -3,12 +3,27 @@ package com.cs315.studentlist2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class JustinRaitzActivity extends AppCompatActivity {
+
+    ListView ideaList;
+
+    // Array of strings...
+    String ideaArray[] = {
+            "Route Creation Running App",
+            "Calorie Tracker App",
+            "Sports Team Camera App"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_justin_raitz);
+
+        ideaList = (ListView)findViewById(R.id.idea_list);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1, ideaArray);
+        ideaList.setAdapter(arrayAdapter);
     }
 }
